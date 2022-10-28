f29ba240e0fd8c15571977612cede1c9863ea25f
AliZainoul <Ali.Zainoul.az@gmail.com>
2022-10-28 12:50:32 +0200

    Adding Package version of the project

---
+++ _Shape/src/Shape.java
@@ -0,0 +1,23 @@
+package src;
+
+
+abstract public class Shape
+{
+  String name;
+
+  public Shape(String _name)  {this.name = _name;}
+
+  public String getName() {return this.name;}
+  public void setName(String _name) {this.name = _name;}
+
+  abstract public double calculateArea();
+}
+
+
+/*
+super(name)!
+override
+abstract
+héritage simple
+encapsulation
+*/
